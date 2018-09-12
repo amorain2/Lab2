53e31b9a2133fb10631f6dd20e8aef4ba6e41bd9
amorain2@illinois.edu <Morain#00>
2018-09-12 12:21:12 -0500

    Maze Work

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,31 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (maze.isFinished() == false) {
+                maze.turnRight();
+                if (maze.canMove()) {
+                    maze.move();
+
+                } else {
+                    maze.turnLeft();
+                    maze.turnLeft();
+                }
+                if (maze.canMove()) {
+                    maze.move();
+
+                } else {
+                    maze.turnRight();
+                }
+                if (maze.canMove()) {
+                    maze.move();
+
+                } else {
+                    maze.turnRight();
+                    maze.turnRight();
+                    maze.move();
+
+                }
+            }
         }
 
         if (maze.isFinished()) {
